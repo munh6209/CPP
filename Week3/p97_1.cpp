8d8fa87517b24e4dd6c118abd72bf68cb40dfa9a
munh6209 <mes7255@chosun.ac.kr>
2026-03-17 21:23:00 +0900

    CPP

--- Week3/p97_1.cpp
+++ Week3/p97_1.cpp
@@ -1,16 +1,60 @@
 #include <iostream>
+using namespace std;
 
-int main()
-{
-    for(int i = 1; i <= 5; ++i)
-    {
-        // std::cout << i << "\n";
-
-        for(int j =1; j <= i; ++j)
-        {
-            std::cout << j;
+int main() {
+    for (int i = 1; i <= 5; ++i) {
+        for (int j = 1; j <= i; ++j) {
+            cout << j;
+            if (j < i) cout << " ";
         }
-
-        std::cout << "\n";
+        cout << '\n';
     }
+    return 0;
+}
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    for (int end = 1; end <= 5; ++end) {
+        for (int j = 5; j >= end; --j){
+            cout <<j;
+            if (j > end) cout << " ";
+        }
+        cout << '\n';
+    }
+    return 0l
+}
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    for (int i = 1; i <= 5; ++i) {
+        for (int s = 0; s < 5 - i; ++s) cout << " ";
+        for (int j = i; j >= 1; --j) {
+            cout << j;
+            if (j > 1) cout << " ";
+        }
+        cout << '\n';
+    }
+    return 0;
+}
+#include <iostream>
+using namespace std;
+
+int main() {
+    for (int i = 1; i <= 5; ++i) {
+        for(int s = 0; s < 5 -i; ++s) cout << " ";
+        for(int j = i; j >= 1; --j) {
+            cout << j;
+            cout << " ";
+        }
+        for (int j = 2; j <= i; ++j) {
+            cout << j;
+            if (j < i) cout << " ";
+        }
+        cout << '\n';
+    }
+    return 0;
 }
